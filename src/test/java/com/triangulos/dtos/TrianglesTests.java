8c27a5848a74479c3fb658ef8698e5800e23c2a1
axeldamian <axel.arrondo@mercadolibre.com>
2024-07-13 20:32:02 -0300

    enhaced test

--- src/test/java/com/triangulos/dtos/TrianglesTests.java
+++ src/test/java/com/triangulos/dtos/TrianglesTests.java
@@ -12,6 +12,8 @@ class TrianglesTests {
     void testingPointsTriangle1() {
         Triangles triangles = new Triangles();
         assertEquals( 6 , triangles.getPointsTriangle1().size() );
+        assertEquals( 6 , triangles.getPointsTriangle2().size() );
+        assertEquals( 6 , triangles.getPointsTriangle3().size() );
     }
     
 }
